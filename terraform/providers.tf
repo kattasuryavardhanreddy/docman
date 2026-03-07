dd99ebc0937495eb148445affc4bfefa77aa3df4
surrreddy <kattasuryavardhan@gmail.com>
2026-03-08 01:31:14 +0530

    Enhance CI/CD workflow: validate backend storage account input, generate container name dynamically, and ensure container existence

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,8 +8,6 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "docmantfstate1234"
-    container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
   }
